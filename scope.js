d42d074fbd0ed0a3a8200bfe3cb947aa962b9bef
partho2589 <parthomalo615@.com>
2022-02-26 00:21:44 +0600

    scope file added

---
+++ scope.js
@@ -0,0 +1,11 @@
+function add ( first , secound){
+    const result = first + secound;
+    console.log(result)
+    if( result > 9){
+        var mood = 'happy'
+         mood = 'cranky'
+    }
+    console.log(mood)
+    return result;
+}
+const sum = add (10, 50)
